da9bb1f50d0fd1c71295fe0786682fca33ef1317
Michael Tu <tuzhucheng@outlook.com>
2018-03-09 01:12:13 -0500

    Fix terminal bug in utils command line args to json

--- utils/utils.py
+++ utils/utils.py
@@ -5,11 +5,28 @@ Utilities
 import json
 
 
+def _parse_arg_val(arg_val):
+    if len(arg_val) > 1:
+        val = ' '.join(arg_val)
+    elif len(arg_val) == 0:
+        val = 1
+    else:  # len(arg_val) == 1
+        try:
+            val = int(arg_val[0])
+        except ValueError:
+            try:
+                val = float(arg_val[0])
+            except ValueError:
+                val = arg_val[0]
+
+    return val
+
+
 def command_line_args_to_json(cmd_args):
     """
     Convert command line args to JSON.
-    >>> command_line_args_to_json(['python', 'main.py', 'model.pkl', '--flag', '--dataset', 'trec', '-u', '1'])
-    '{"program": "python", "main": "main.py", "arg2": "model.pkl", "flag": 1, "dataset": "trec"}'
+    >>> command_line_args_to_json(['python', 'main.py', 'model.pkl', '--flag', '--dataset', 'trec', '-u', '1', '-t', '1.23'])
+    '{"program": "python", "main": "main.py", "arg2": "model.pkl", "flag": 1, "dataset": "trec", "u": 1, "t": 1.23}'
     """
     d = {}
     if len(cmd_args) < 2:
@@ -31,25 +48,15 @@ def command_line_args_to_json(cmd_args):
     while i < len(cmd_args):
         if cmd_args[i].startswith('-'):
             if arg_name is not None:
-                if len(arg_val) > 1:
-                    val = ' '.join(arg_val)
-                elif len(arg_val) == 0:
-                    val = 1
-                else:  # len(arg_val) == 1
-                    try:
-                        val = int(arg_val[0])
-                    except ValueError:
-                        try:
-                            val = float(arg_val[0])
-                        except:
-                            val = arg_val[0]
-
-                d[arg_name] = val
+                d[arg_name] = _parse_arg_val(arg_val)
+                arg_val = []
 
             arg_name = cmd_args[i].lstrip('-')
         else:
             arg_val.append(cmd_args[i])
 
         i += 1
+
+    d[arg_name] = _parse_arg_val(arg_val)
 
     return json.dumps(d)

521bc1545977471b7e3af0262ff83b6d5f26735e
Michael Tu <tuzhucheng@outlook.com>
2018-03-09 01:14:23 -0500

    Persist all tasks to sql database

--- db/__init__.py
+++ db/__init__.py
@@ -10,6 +10,6 @@ if res.fetchone() is None:
     c.execute('''CREATE TABLE experiment_groups(name text, description text)''')
     c.execute('''INSERT INTO experiment_groups(name, description) VALUES ('adhoc', 'ad-hoc experiment group')''')
 
-    c.execute('''CREATE TABLE experiments(group_id integer, args text, stdout text, stderr text,
+    c.execute('''CREATE TABLE experiments(group_id integer, args text, stdout text, stderr text, status_code int,
                   FOREIGN KEY(group_id) REFERENCES experiment_groups(rowid))''')
     conn.commit()
--- task_queue/tasks.py
+++ task_queue/tasks.py
@@ -1,22 +1,35 @@
 import pathlib
 import subprocess
 
+from db import conn
 from task_queue.celery import app
+from utils.utils import command_line_args_to_json
 
 
 @app.task
-def run_model(python, driver, cmd_args):
+def run_model(cmd_args, experiment_group='adhoc'):
     request_id = run_model.request.id
 
     pathlib.Path('log').mkdir(exist_ok=True)
-    stdout_log, stderr_log = f'log/{request_id}-stdout.log', f'log/{request_id}-stderr.log'
+    cursor = conn.cursor()
+    expt_group_row = cursor.execute('SELECT rowid FROM experiment_groups WHERE name=?', (experiment_group,)).fetchone()
+    if expt_group_row is None:
+        raise RuntimeError('Experiment group {} does not exist'.format(expt_group_row))
+    expt_group_id = expt_group_row[0]
+
+    stdout_log_name, stderr_log_name = f'{request_id}-stdout.log', f'{request_id}-stderr.log'
+    stdout_log, stderr_log = f'log/{stdout_log_name}', f'log/{stderr_log_name}'
     print('Processing request', request_id)
     stdout_fh, stderr_fh = open(stdout_log, 'w'), open(stderr_log, 'w')
 
-    call_args = [python, driver] + cmd_args
-    completed = subprocess.run(call_args, stdout=stdout_fh, stderr=stderr_fh)
+    completed = subprocess.run(cmd_args, stdout=stdout_fh, stderr=stderr_fh)
     stdout_fh.close()
     stderr_fh.close()
+
+    args_json = command_line_args_to_json(cmd_args)
+    cursor.execute('INSERT INTO experiments(group_id, args, stdout, stderr, status_code) VALUES (?, ?, ?, ?, ?)',
+                   (expt_group_id, args_json, stderr_log_name, stderr_log_name, completed.returncode))
+    conn.commit()
 
     return {
         'status_code': completed.returncode,

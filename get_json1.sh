34582c560867cca1139084613ba92bc3210f6c04
Michael Tu <tuzhucheng@outlook.com>
2018-03-09 13:39:07 -0500

    get json1 script diff behaviour on diff systems

--- get_json1.sh
+++ get_json1.sh
@@ -2,4 +2,8 @@
 
 curl -O http://sqlite.org/2016/sqlite-src-3140100.zip
 unzip sqlite-src-3140100.zip
-gcc -g -fPIC -dynamiclib sqlite-src-3140100/ext/misc/json1.c -o json1
+if [ "$(uname)" == "Darwin" ]; then
+    gcc -g -fPIC -dynamiclib sqlite-src-3140100/ext/misc/json1.c -o json1
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    gcc -g -fPIC -shared sqlite-src-3140100/ext/misc/json1.c -o json1.so
+fi
